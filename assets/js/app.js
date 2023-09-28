52dc077502c22c1a0ae9b4ee6899e9082a259307
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-09-28 18:32:45 +0530

    adding tabs with js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,11 @@
-// const tabsContainer = document.getElementById("tabs");
-// console.log(tabsContainer);
-// const tabTogglers = document.querySelectorAll("#tabs a");
-// console.log(tabTogglers);
-
-// tabTogglers.forEach(function (toggler) {
-//     toggler.addEventListener("click", function (e) {
-//         e.preventDefault;
-
-//         let tabName = this.getAttribute("href");
-//         console.log(tabName);
-//         let tabContents = document.querySelector("#tab-contents");
-
-//         for (let i = 0; i < tabContents.children.length; i++) {
-//             tabTogglers[i].parentElement.classList.remove("border-b-4");
-//             tabContents.children[i].classList.remove("hidden");
-//             if ("#" + tabContents.children[i].id === tabName) {
-//                 continue;
-//             }
-//             tabContents.children[i].classList.add("hidden");
-
-//         }
-//         e.target.parentElement.classList.add("border-b-4");
-//     });
-
-// });
 
 let tabsContainer = document.querySelector("#tabs");
 
 let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
+
+let tabSelect = tabsContainer.nextElementSibling
+
+// console.log(tabSelect);
 
 console.log(tabTogglers);
 
@@ -35,13 +13,14 @@ tabTogglers.forEach(function (toggler) {
     toggler.addEventListener("click", function (e) {
         e.preventDefault();
 
+
         let tabName = this.getAttribute("href");
 
         let tabContents = document.querySelector("#tab-contents");
 
         for (let i = 0; i < tabContents.children.length; i++) {
-
-            tabTogglers[i].parentElement.classList.remove("border-b-4");
+            console.log(tabTogglers[i].nextElementSibling);
+            tabTogglers[i].nextElementSibling.classList.remove("bg-white", "w-full", "h-1", "absolute", "bottom-0", "top-6");
             tabContents.children[i].classList.remove("hidden");
             if ("#" + tabContents.children[i].id === tabName) {
                 continue;
@@ -49,6 +28,7 @@ tabTogglers.forEach(function (toggler) {
             tabContents.children[i].classList.add("hidden");
 
         }
-        e.target.parentElement.classList.add("border-b-4");
+
+        e.target.nextElementSibling.classList.add("bg-white", "w-full", "h-1", "absolute", "bottom-0", "top-6");
     });
 });
