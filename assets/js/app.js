f12d621f225a0b273122b904aed9d9a281f3b592
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-10-24 08:31:15 +0530

    difference  external page

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,40 +17,40 @@ dotButton.addEventListener("click", () => {
 });
 
 
-// tab system
-let tabsContainer = document.querySelector("#tabs");
+// // tab system
+// let tabsContainer = document.querySelector("#tabs");
 
-let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
+// let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
 
-let tabSelect = tabsContainer.nextElementSibling
+// let tabSelect = tabsContainer.nextElementSibling
 
-// console.log(tabSelect);
+// // console.log(tabSelect);
 
-console.log(tabTogglers);
+// console.log(tabTogglers);
 
-tabTogglers.forEach(function (toggler) {
-    toggler.addEventListener("click", function (e) {
-        e.preventDefault();
+// tabTogglers.forEach(function (toggler) {
+//     toggler.addEventListener("click", function (e) {
+//         e.preventDefault();
 
 
-        let tabName = this.getAttribute("href");
+//         let tabName = this.getAttribute("href");
 
-        let tabContents = document.querySelector("#tab-contents");
+//         let tabContents = document.querySelector("#tab-contents");
 
-        for (let i = 0; i < tabContents.children.length; i++) {
-            console.log(tabTogglers[i].nextElementSibling);
-            tabTogglers[i].nextElementSibling.classList.remove("bg-white", "w-full", "h-1", "absolute", "bottom-0", "top-6");
-            tabContents.children[i].classList.remove("hidden");
-            if ("#" + tabContents.children[i].id === tabName) {
-                continue;
-            }
-            tabContents.children[i].classList.add("hidden");
+//         for (let i = 0; i < tabContents.children.length; i++) {
+//             console.log(tabTogglers[i].nextElementSibling);
+//             tabTogglers[i].nextElementSibling.classList.remove("bg-white", "w-full", "h-1", "absolute", "bottom-0", "top-6");
+//             tabContents.children[i].classList.remove("hidden");
+//             if ("#" + tabContents.children[i].id === tabName) {
+//                 continue;
+//             }
+//             tabContents.children[i].classList.add("hidden");
 
-        }
+//         }
 
-        e.target.nextElementSibling.classList.add("bg-white", "w-full", "h-1", "absolute", "bottom-0", "top-6");
-    });
-});
+//         e.target.nextElementSibling.classList.add("bg-white", "w-full", "h-1", "absolute", "bottom-0", "top-6");
+//     });
+// });
 
 //viewd updates toggle
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       backgroundImage: {
         'chatarea-background': "url('https://e0.pxfuel.com/wallpapers/722/149/desktop-wallpaper-message-background-whatsapp-message-background-thumbnail.jpg')",
 
+      },
+      gridTemplateColumns: {
+        'tablist-grid': '0.5fr 1fr 1fr 1fr'
       }
     },
   },
